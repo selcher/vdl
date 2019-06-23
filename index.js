ef119d76ede8079a708cb64db7a5eccad4ab8130
Selcher <selcher123@gmail.com>
2019-06-23 17:13:54 +0900

    Separate code blocks into tasks

--- index.js
+++ index.js
@@ -3,42 +3,46 @@
 const log = console.log;
 const msg = require('./src/messages');
 
+/**
+ * Tasks
+ */
+let tasks = {};
+
+/**
+ * Task: Initialize command-line interface
+ */
 const terminal = require('./src/terminal');
 const package = require('./package.json');
 const name = package.name;
 const version = package.version;
-const program = terminal.init({
-    name,
-    version,
-    usage: msg.usage,
-    help: () => {
-        log(msg.examples(name));
-    }
-});
 
-const done = () => {
+tasks.init = () => (
+    terminal.init({
+        name,
+        version,
+        usage: msg.usage,
+        help: () => {
+            log(msg.examples(name));
+        }
+    })
+);
+
+/**
+ * Task: Close command-line interface
+ */
+tasks.done = () => {
     log(msg.done);
     terminal.close();
 };
 
+/**
+ * Task: Download Video From Url
+ */
+
 const vid = require('./src/video');
-const fs = require('fs');
 
-let commandFound = false;
-
-const onCommandFound = () => {
-    commandFound = true;
-    log(msg.about(name));
-};
-
-if (program.lang) {
-    vid.setLanguage(program.lang);
-}
-
-if (program.url) {
-    onCommandFound();
-
-    vid.downloadFromUrl(program.url)
+tasks.downloadFromUrl = (url) => {
+    vid.downloadFromUrl(url)
         .then(videoDownloadedMsg => log(videoDownloadedMsg))
         .catch((err) => {
             if (program.error) {
@@ -48,32 +52,38 @@ if (program.url) {
                 log(msg.errDownloading);
             }
         })
-        .finally(done);
-}
+        .finally(tasks.done);
+};
 
-if (program.file) {
-    onCommandFound();
+/**
+ * Task: Download Videos Listed in File
+ */
 
-    const filePath = program.file;
+const fs = require('fs');
+const getFileContentAsList = (pathToFile) => {
+    const newLineChars = /\r\n/g;
+    const invalidChars = /\t\r\n\v\f/g;
+    const content = fs.readFileSync(pathToFile, 'utf-8');
+    let fileContentList = content.replace(newLineChars, '\n')
+        .split('\n')
+        .map(url => url ? url.replace(invalidChars, '') : '')
+        .filter(url => url);
 
+    return fileContentList;
+};
+
+tasks.downloadFromFile = (filePath) => {
     log(msg.readFile(filePath));
-
-    const getFileContentAsList = pathToFile => {
-        const newLineChars = /\r\n/g;
-        const invalidChars = /\t\r\n\v\f/g;
-        const content = fs.readFileSync(pathToFile, 'utf-8');
-        let fileContentList = content.replace(newLineChars, '\n')
-            .split('\n')
-            .map(url => url ? url.replace(invalidChars, '') : '')
-            .filter(url => url);
-
-        return fileContentList;
-    };
 
     const contentList = getFileContentAsList(filePath);
     const totalItems = contentList.length;
 
     log(msg.itemsFound(totalItems));
+
+    if (totalItems === 0) {
+        tasks.done();
+        return;
+    }
 
     const logCurrentItemOfTotalItems = ((total) => {
         let counter = 1;
@@ -121,16 +131,38 @@ if (program.file) {
             searchAndDownload(nextUrl);
         }
         else {
-            done();
+            tasks.done();
         }
     };
 
-    if (totalItems) {
-        searchAndDownload(contentList.shift());
-    }
-    else {
-        done();
-    }
+    searchAndDownload(contentList.shift());
+};
+
+/**
+ * Execute command
+ */
+
+let commandFound = false;
+
+const onCommandFound = () => {
+    commandFound = true;
+    log(msg.about(name));
+};
+
+const program = tasks.init();
+
+if (program.lang) {
+    vid.setLanguage(program.lang);
+}
+
+if (program.url) {
+    onCommandFound();
+    tasks.downloadFromUrl(program.url);
+}
+
+if (program.file) {
+    onCommandFound();
+    tasks.downloadFromFile(program.file);
 }
 
 if (!commandFound) {
